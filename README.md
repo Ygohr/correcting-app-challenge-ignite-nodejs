bef77f48927d754a1c3ab21cbfaf5e03fc9fcff0
Ygohr <medeirosygohr@gmail.com>
2021-03-15 22:14:23 -0300

    adding challenge files

---
+++ README.md
@@ -0,0 +1,43 @@
+# ⚔️ Ignite - Desafio 3: Corrigindo Bugs/Problemas
+![](assets/capa_ignite.png)
+<br>
+
+## :pushpin: Sobre o Desafio
+Este desafio tem como objetivo a correção de uma aplicação que está em processo de desenvolvimento mas que já possui os testes necessários para fazer toda a validação dos requisitos.
+Essa aplicação realiza o **CRUD** (Create, Read, Update, Delete) de repositórios de **projetos**. Além disso, é possível dar _likes_ em repositórios cadastrados, aumentando a quantidade de likes em 1 a cada vez que a rota é chamada.
+
+O _template_ de arquivos utilizados neste projeto pode ser acessado em: [**template.**](https://github.com/rocketseat-education/ignite-template-corrigindo-o-codigo)
+
+## 📋 Requisitos e Regras de Negócio
+O documento de referência de Requisitos e Regras de Negócio pode acessado através deste [**link.**](https://www.notion.so/Requisitos-e-Regras-de-Neg-cio-Corrigindo-Bugs-Problemas-Desafio-3-Ignite-fd8fc60292394f11855e568edc156ef3)
+
+## 💻 Instalação, Dependências e Executando o Projeto
+**1.** Clone este repositório 
+```
+git clone https://github.com/Ygohr/correcting-app-challenge-ignite-nodejs
+``` 
+**2.** Vá até o diretório raiz do projeto
+```
+cd correcting-app-challenge-ignite-nodejs
+``` 
+**3.** Instale as dependências necessárias
+```
+yarn 
+ou
+npm install
+```
+**4.** Execute a aplicação
+```
+yarn dev
+```
+
+## :floppy_disk: Usando a Aplicação
+Para realização de **requisições** na aplicação, deve ser utilizado o **Insomnia** ou **Postman**, importando o [arquivo](insomnia_requests) de requisições já existente neste diretório.
+Atentar-se aos parâmetros necessários no **Header**.
+
+## :syringe: Evidência de Testes
+Nesta aplicação, são contemplados **Testes Unitários**, visando garantir o correto funcionamento das funcionalidades e manter a aplicação de acordo com os requisitos. <br/>
+![](assets/test_evidence.png)
+
+
+- **Desenvolvido** **por** [**Ygohr**](https://www.linkedin.com/in/ygohr-medeiros-28451b14a/) 🤖
--- src/index.js
+++ src/index.js
@@ -8,12 +8,22 @@ app.use(express.json());
 
 const repositories = [];
 
+function checkRepositoryExistency(request, response, next) {
+  const { id } = request.params;
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found!" });
+  }
+  request.repositoryIndex = repositoryIndex;
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
+  const { title, url, techs } = request.body;
 
   const repository = {
     id: uuid(),
@@ -23,20 +33,16 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository);
+
   return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
-  const updatedRepository = request.body;
+app.put("/repositories/:id", checkRepositoryExistency, (request, response) => {
+  const { url, title, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
-
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const { repositoryIndex } = request;
+  const repository = { ...repositories[repositoryIndex], title, url, techs };
 
   repositories[repositoryIndex] = repository;
 
@@ -48,7 +54,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -57,18 +63,12 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
+app.post("/repositories/:id/like", checkRepositoryExistency, (request, response) => {
+  const { repositoryIndex } = request;
+  const repository = repositories[repositoryIndex]
+  repository.likes++;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
-
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  return response.json(repository);
 });
 
 module.exports = app;
